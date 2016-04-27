5fa864b4e16011f77a33634412567774fa626f8f
vukbgit <info@vuk.bg.it>
2016-04-27 19:11:53 +0200

    added support for goodby/csv

---
+++ src/CsvGoodby.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PHPCraft\Csv;
+
+use Goodby\CSV\Export\Standard\Exporter;
+use Goodby\CSV\Export\Standard\ExporterConfig;
+
+/**
+ * Reads/creates CSV
+ *
+ * @author vuk <info@vuk.bg.it>
+ */
+class CsvGoodby implements CsvInterface
+{
+    private $delimiter = ',';
+    
+    /**
+     * Sets delmiter
+     *
+     * @param string $delimiter
+     **/
+    public function setDelimiter($delimiter)
+    {
+        $this->delimiter = $delimiter;
+    }
+    
+    /**
+     * creates and outputs CSV from array of objects
+     *
+     * @param string $name file name (without csv extension)
+     * @param array $records
+     **/
+    public function fromObjects($name, array $records){
+        $config = new ExporterConfig();
+        $config->setDelimiter($this->delimiter);
+        $exporter = new Exporter($config);
+        //$writer->output($name.'.csv');
+        $exporter->export('php://output',$records);
+    }
+}
