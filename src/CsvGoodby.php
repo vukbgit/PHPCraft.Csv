fd025f4946a827c5c70547d3f6fd16f1fe0ea1c9
vukbgit <info@vuk.bg.it>
2016-04-28 11:02:10 +0200

    added column headers

--- src/CsvGoodby.php
+++ src/CsvGoodby.php
@@ -13,6 +13,7 @@ use Goodby\CSV\Export\Standard\ExporterConfig;
 class CsvGoodby implements CsvInterface
 {
     private $delimiter = ',';
+    private $columnHeaders;
     
     /**
      * Sets delmiter
@@ -25,18 +26,45 @@ class CsvGoodby implements CsvInterface
     }
     
     /**
+     * Sets column headers
+     *
+     * @param array $columnHeaders
+     **/
+    public function setColumnHeaders($columnHeaders)
+    {
+        $this->columnHeaders = $columnHeaders;
+    }
+    
+    /**
      * creates and outputs CSV from array of objects
      *
      * @param string $name file name (without csv extension)
      * @param array $records
      * @return string csv content
      **/
-    public function fromObjects($name, array $records){
+    public function fromObjects($name, array $records)
+    {
         $config = new ExporterConfig();
         $config->setDelimiter($this->delimiter);
+        if($this->columnHeaders) $config->setColumnHeaders($this->columnHeaders);
         $exporter = new Exporter($config);
         ob_start();
         $exporter->export('php://output', $records);
         return ob_get_clean();
+    }
+    
+    /**
+     * builds HTTP headers for file open/download
+     *
+     * @param string $name file name (without csv extension)
+     * @return associative array indexed by headers names and whose elements are headers values
+     **/
+    public function buildHttpHeaders($name)
+    {
+        return [
+            'Content-Type' => 'text/csv',
+            'Content-Transfer-Encoding' => 'binary',
+            'Content-Disposition' => 'attachment; filename="' . $name . '.csv"'
+        ];
     }
 }
--- src/CsvInterface.php
+++ src/CsvInterface.php
@@ -17,11 +17,26 @@ interface CsvInterface
     public function setDelimiter($delimiter);
     
     /**
+     * Sets column headers
+     *
+     * @param array $columnHeaders
+     **/
+    public function setColumnHeaders($columnHeaders);
+    
+    /**
      * creates and outputs CSV from array of objects
      *
      * @param string $name file name (without csv extension)
      * @param array $records
      * @return string csv content
      **/
-    public function fromObjects($name, array $records);    
+    public function fromObjects($name, array $records);
+    
+    /**
+     * builds HTTP headers for file open/download
+     *
+     * @param string $name file name (without csv extension)
+     * @return array of associative arrays with keys 'name' and 'value'
+     **/
+    public function buildHttpHeaders($name);
 }
